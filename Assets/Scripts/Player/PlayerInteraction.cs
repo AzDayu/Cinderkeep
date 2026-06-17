d0b325a48f67409682fecdbf4bc3759d24d1a060
Felia <felia1564@gmail.com>
2026-06-17 18:44:11 +0900

    자원용 레이어 추가 + 플레이어 상호작용 코드

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -5,5 +5,26 @@ public class PlayerInteraction : MonoBehaviour
     [Header("상호작용 세팅")]
     [SerializeField] private float _interactionDistance = 3f; // 상호작용 거리
     [SerializeField] private LayerMask _interactionLayerMask; // 상호작용 레이어 마스크
-    [SerializeField] private Transform _camera; // 플레이어 카메라
+    [SerializeField] private Transform Transform_Camera; // 플레이어 카메라
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryInteract();
+        }
+    }
+
+    private void TryInteract()
+    {
+        Ray ray = new Ray(Transform_Camera.position, Transform_Camera.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, _interactionDistance, _interactionLayerMask))
+        {
+            //GameObject hitObject = hitInfo.collider.gameObject;
+            //var fieldObject = hitObject.LayerMask.NameToLayer("Resources");
+
+        }
+    }
 }
