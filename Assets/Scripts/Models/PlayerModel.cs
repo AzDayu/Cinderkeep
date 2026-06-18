367af68490bae290832141be4da890b53df6f913
김성광 <rweqtwed@gmail.com>
2026-06-18 18:05:20 +0900

    [3.13] 이벤트 기반 자원 관리 시스템 및 실시간 UI 연동 미리 구현
    작업자: 성광
    브랜치: feature/seonggwang-player-model
    
    작업 내용:
    - PlayerModel 내부에 5종의 신규 자원(Wood, Stone, Iron, Mithril, Adamantium) 데이터 구조 추가
    - 자원 획득(AddResource) 및 소모(UseResource)를 처리하는 캡슐화된 비즈니스 로직 구현
    - 자원 사용 시 수량이 충분하여 차감에 성공했을 때만 이벤트를 발생시키도록 switch문 구조 최적화
    - 데이터 변동 시에만 UI를 갱신하도록 Action 델리게이트(OnResourceChanged)를 활용한 이벤트 주도(Event-Driven) 방식 도입
    - ResourceUI 스크립트 추가 및 옵저버 패턴을 통한 TMP_Text 슬롯 실시간 동기화 처리
    - 씬 전환이나 오브젝트 파괴 시 메모리 누수가 발생하지 않도록 OnDestroy에 이벤트 구독 해제(Unsubscribe) 안전장치 적용
    
    주의 사항:
    
     - 추후 playerinteract를 통해 연동필요

--- Assets/Scripts/Models/PlayerModel.cs
+++ Assets/Scripts/Models/PlayerModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 namespace Cinderkeep.Gameplay
 {
@@ -9,37 +10,93 @@ namespace Cinderkeep.Gameplay
     {
         private int _health;
         private int _maxHealth;
+        private int _stamina;
+        private int _maxStamina;
         private int _level;
 
-        public int Health
-        {
-            get
-            {
-                return _health;
-            }
-        }
 
-        public int MaxHealth
-        {
-            get
-            {
-                return _maxHealth;
-            }
-        }
+        private int _wood;
+        private int _stone;
+        private int _iron;
+        private int _mithril;
+        private int _adamantium;
 
-        public int Level
-        {
-            get
-            {
-                return _level;
-            }
-        }
+
+        public int Health { get { return _health; } }
+        public int MaxHealth { get { return _maxHealth; } }
+        public int Stamina { get { return _stamina; } }
+        public int MaxStamina { get { return _maxStamina; } }
+        public int Level { get { return _level; } }
+
+
+        public int Wood { get { return _wood; } }
+        public int Stone { get { return _stone; } }
+        public int Iron { get { return _iron; } }
+        public int Mithril { get { return _mithril; } }
+        public int Adamantium { get { return _adamantium; } }
+
+
+        public event Action OnResourceChanged;
+
 
         public void InitializeDefault()
         {
             _maxHealth = 100;
             _health = _maxHealth;
             _level = 1;
+
+            _wood = 0;
+            _stone = 0;
+            _iron = 0;
+            _mithril = 0;
+            _adamantium = 0;
         }
+
+
+        public void AddResource(string resourceType, int amount)
+        {
+            if (resourceType == "Stone") _stone += amount;
+            else if (resourceType == "Wood") _wood += amount;
+            else if (resourceType == "Iron") _iron += amount;
+            else if (resourceType == "Mithril") _mithril += amount;
+            else if (resourceType == "Adamantium") _adamantium += amount;
+
+            OnResourceChanged?.Invoke();
+        }
+
+        public bool UseResource(string resourceType, int amount)
+        {
+            bool isSuccess = false;
+            switch (resourceType)
+            {
+                case "Wood":
+                    if (_wood >= amount) { _wood -= amount; isSuccess = true; }
+                    break;
+                case "Stone":
+                    if (_stone >= amount) { _stone -= amount; isSuccess = true; }
+                    break;
+                case "Iron":
+                    if (_iron >= amount) { _iron -= amount; isSuccess = true; }
+                    break;
+                case "Mithril":
+                    if (_mithril >= amount) { _mithril -= amount; isSuccess = true; }
+                    break;
+                case "Adamantium":
+                    if (_adamantium >= amount) { _adamantium -= amount; isSuccess = true; }
+                    break;
+            }
+
+            if (isSuccess == true)
+            {
+                OnResourceChanged?.Invoke();
+            }
+
+            return isSuccess;
+        }
+
+
+
+
+
     }
 }
---
+++ Assets/Scripts/UI/ResourceUI.cs
@@ -0,0 +1,41 @@
+﻿?using UnityEngine;
+using TMPro;
+using Cinderkeep.Gameplay;
+
+public sealed class ResourceUI : MonoBehaviour
+{
+    [Header("자원 텍스트 UI 연결")]
+    [SerializeField] private TMP_Text _woodText;
+    [SerializeField] private TMP_Text _stoneText;
+    [SerializeField] private TMP_Text _ironText;
+    [SerializeField] private TMP_Text _mithrilText;
+    [SerializeField] private TMP_Text _adamantiumText;
+
+    private void Start()
+    {
+        if (GameManager.Inst != null && GameManager.Inst.PlayerModel != null)
+        {
+            GameManager.Inst.PlayerModel.OnResourceChanged += UpdateResourceUI;
+
+            UpdateResourceUI();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (GameManager.Inst != null && GameManager.Inst.PlayerModel != null)
+        {
+            GameManager.Inst.PlayerModel.OnResourceChanged -= UpdateResourceUI;
+        }
+    }
+
+    private void UpdateResourceUI()
+    {
+        PlayerModel model = GameManager.Inst.PlayerModel;
+        if (_woodText != null) _woodText.text = model.Wood.ToString();
+        if (_stoneText != null) _stoneText.text = model.Stone.ToString();
+        if (_ironText != null) _ironText.text = model.Iron.ToString();
+        if (_mithrilText != null) _mithrilText.text = model.Mithril.ToString();
+        if (_adamantiumText != null) _adamantiumText.text = model.Adamantium.ToString();
+    }
+}
